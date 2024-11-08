3e32bbdc3aded49ebb7ed5372e4592b872cc09ff
zsomboXD <bozsombo@gmail.com>
2024-11-08 13:46:19 +0100

    database config

--- configDB.js
+++ configDB.js
@@ -1,7 +1,20 @@
-export const configDB={
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const configDB_={
     host:'localhost',
     user:'root',
     password:'',
     database:'todos',
+    multipleStatements:true//engedélyezünk több útasítást
+}
+
+export const configDB={
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
+    port:process.env.DB_PORT,
     multipleStatements:true//engedélyezünk több útasítást
 }
